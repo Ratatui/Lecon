031098907e708d66ac3ae25e9ffd25d5984c1175
Stas Frolov <ratatui4ik@gmail.com>
2013-05-13 16:28:43 +0300

    Memory, Motherboard

---
+++ Lecon/ViewModels/Memory_ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Lecon.Web;
+using System;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Collections;
+using System.ComponentModel;
+
+namespace Lecon.ViewModels
+{
+	public class Memory_ViewModel : ViewModel
+	{
+		public ICollectionView MemoriesView { get; set; }
+		public ICollectionView ManufacturersView { get; set; }
+		public ICollectionView MemoryTypesView { get; set; }
+
+		public Memory_ViewModel()
+			: base()
+		{
+			Context.Load(Context.GetMemoryTypesQuery());
+			Context.Load(Context.GetMemorysQuery());
+			Context.Load(Context.GetManufacturersQuery());
+
+			this.MemoryTypesView = this.CreateView(Context.MemoryTypes);
+			this.MemoriesView = this.CreateView(Context.Memories);
+			this.ManufacturersView = this.CreateView(Context.Manufacturers);
+		}
+
+		protected override void OnAddCommand()
+		{
+			var newMemory = new Memory();
+			var newDevice = new Device();
+			newMemory.Device = newDevice;
+
+			this.Context.Memories.Add(newMemory);
+			this.MemoriesView.MoveCurrentTo(newMemory);
+		}
+
+		protected override void OnRefreshCommand()
+		{
+		}
+
+		protected override void OnDeleteCommand()
+		{
+		}
+	}
+}
---
+++ Lecon/ViewModels/Motherboard_ViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Lecon.Web;
+using System;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Collections;
+using System.ComponentModel;
+
+namespace Lecon.ViewModels
+{
+	public class Motherboard_ViewModel : ViewModel
+	{
+		public ICollectionView PowerTypesView { get; set; }
+		public ICollectionView ChipsetsView { get; set; }
+		public ICollectionView ManufacturersView { get; set; }
+		public ICollectionView SocketsView { get; set; }
+		public ICollectionView MemoryTypesView { get; set; }
+		public ICollectionView FormFactorsView { get; set; }
+		public ICollectionView MotherboardsView { get; set; }
+
+		public Motherboard_ViewModel()
+			: base()
+		{
+			Context.Load(Context.GetPowerTypesQuery());
+			Context.Load(Context.GetChipsetsQuery());
+			Context.Load(Context.GetManufacturersQuery());
+			Context.Load(Context.GetMemoryTypesQuery());
+			Context.Load(Context.GetMotherboardsQuery());
+			Context.Load(Context.GetFormFactorsQuery());
+			Context.Load(Context.GetSocketsQuery());
+
+			this.PowerTypesView = this.CreateView(Context.PowerTypes);
+			this.ChipsetsView = this.CreateView(Context.Chipsets);
+			this.MemoryTypesView = this.CreateView(Context.MemoryTypes);
+			this.MotherboardsView = this.CreateView(Context.Motherboards);
+			this.FormFactorsView = this.CreateView(Context.FormFactors);
+			this.ManufacturersView = this.CreateView(Context.Manufacturers);
+			this.SocketsView = this.CreateView(Context.Sockets);
+		}
+
+		protected override void OnAddCommand()
+		{
+			var newMotherboard = new Motherboard();
+			var newDevice = new Device();
+			newMotherboard.Device = newDevice;
+
+			this.Context.Motherboards.Add(newMotherboard);
+			this.MotherboardsView.MoveCurrentTo(newMotherboard);
+		}
+
+		protected override void OnRefreshCommand()
+		{
+		}
+
+		protected override void OnDeleteCommand()
+		{
+		}
+	}
+}
